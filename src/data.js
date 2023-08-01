be260ad0dade9bf34e30ed81a9c602d70e25e7c5
linards <linardssils1@gmail.com>
2023-08-02 01:02:22 +0300

    Adding some things

---
+++ src/data.js
@@ -0,0 +1,23 @@
+const data = {
+  products: [
+    {
+      name: 'Banana',
+      image: '/images/banana.jpg',
+      slug: 'banana',
+      protein: 1.1,
+    },
+    {
+      name: 'Apple',
+      image: '/images/apples.jpg',
+      slug: 'apple',
+      protein: 0.3,
+    },
+    {
+      name: 'Egg',
+      image: '/images/eggs.jpg',
+      slug: 'egg',
+      protein: 6,
+    },
+  ],
+};
+export default data;
